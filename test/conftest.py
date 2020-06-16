e9918c44ee4887d70c9034a20c4af3462a9e172c
lpoplavska <liubov.poplavska@globallogic.com>
2020-06-16 13:29:07 +0300

    tests run using browserstack

--- test/conftest.py
+++ test/conftest.py
@@ -8,7 +8,20 @@ driver = None
 def setup_teardown(request):
     global driver
     print('setup method')
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    BROWSERSTACK_URL = 'https://liubov14:pxapctrFnTmgLdoUE5yi@hub-cloud.browserstack.com/wd/hub'
+    desired_cap = {
+
+        'os': 'Windows',
+        'os_version': '8.1',
+        'browser': 'Chrome',
+        'browser_version': '78',
+        'name': "liubov14's First Test",
+        'download.default_directory': "/tmp/"
+    }
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('--start-maximized')
+    driver = webdriver.Remote(command_executor=BROWSERSTACK_URL, desired_capabilities=desired_cap, options=options)
     request.cls.driver = driver
     yield
     print('teardown method')
